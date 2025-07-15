b2e71916edb2d806aececb15c4f16ca863e69900
fr-Ivy <dejongivy@gmail.com>
2025-07-15 16:43:06 +0200

    first actual save (I started)

--- game.cpp
+++ game.cpp
@@ -4,26 +4,23 @@
 
 namespace Tmpl8
 {
-	// -----------------------------------------------------------
-	// Initialize the application
-	// -----------------------------------------------------------
+	//assets
+	Sprite cat_forward(new Surface("assets/the_cat_pack/Characters/cat_forward.png"), 4);
+
 	void Game::Init()
 	{
 	}
 	
-	// -----------------------------------------------------------
-	// Close down application
-	// -----------------------------------------------------------
+
 	void Game::Shutdown()
 	{
-		//
 	}
 
 
-	// -----------------------------------------------------------
-	// Main application tick function
-	// -----------------------------------------------------------
+
 	void Game::Tick(float deltaTime)
 	{
+		screen->Clear(0);
+		cat_forward.Draw(screen, 50, 50);
 	}
 };
